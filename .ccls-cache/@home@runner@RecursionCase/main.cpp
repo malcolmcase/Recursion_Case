0e04b64a28824d9c3c1051c1c6c49c7df992afdc
Malcolm Case <102632901+malcolmcase@users.noreply.github.com>
2022-05-04 16:39:46 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@RecursionCase/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void towerOfHanoi(int n, char fromRod, char toRod, char exRod)
+{
+  if (n == 0)
+  {
+    return;
+  }
+  else
+  {
+    towerOfHanoi(n - 1, fromRod, exRod, toRod);
+    cout << "Move disk " << n << " from rod " << fromRod << " to rod " << toRod << endl;
+    towerOfHanoi(n - 1, exRod, toRod, fromRod);
+  }
+}
+
+void recursion(int n, int x)
+{ 
+  if (n==0)
+  {
+    cout << x;
+    return;
+  }
+  else
+  {
+    x = x*n ;
+    recursion(n-1, x);
+    return;
+  }
+}
+
+int main()
+{
+  int n;
+  int x = 1;
+  cout << "Please enter number of rods for Tower of Hanoi\n";
+  cin >> n;
+  cout << endl;
+  towerOfHanoi(n, 'A', 'C', 'B');
+
+  //Recursion is
+  
+  cout << endl << "Recursion problem: ";
+  for ( int i = 0; i < n; i++ )
+    {
+      cout << n-i;
+      if (i != n-1)
+      {
+        cout << " * ";
+      }
+      else
+      {
+        cout << " = ";
+      }
+    }
+  recursion(n, x);
+  cout << endl;
+
+  return 0;
+}
